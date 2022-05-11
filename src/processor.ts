55c42a6881686857f3a2ff9e5d34ff1d9982a0d9
Massimo Luraschi <massimo.lura@gmail.com>
2022-05-11 10:27:05 +0200

    Adapted template to fit Orcus use case

--- src/processor.ts
+++ src/processor.ts
@@ -1,78 +1,38 @@
-import {
-  EvmLogHandlerContext,
-  SubstrateEvmProcessor,
-} from "@subsquid/substrate-evm-processor";
+import { EvmLogHandlerContext, SubstrateEvmProcessor } from "@subsquid/substrate-evm-processor";
 import { lookupArchive } from "@subsquid/archive-registry";
-import { CHAIN_NODE, contract, createContractEntity, getContractEntity } from "./contract";
-import * as erc721 from "./abi/erc721";
-import { Owner, Token, Transfer } from "./model";
+import { events } from "./abi/bank-safe";
+import { Investement } from "./model";
 
-const processor = new SubstrateEvmProcessor("moonriver-substrate");
+const processor = new SubstrateEvmProcessor("astar-substrate");
 
 processor.setBatchSize(500);
 
 processor.setDataSource({
-  chain: CHAIN_NODE,
-  archive: lookupArchive("moonriver")[0].url,
+  chain: "wss://astar.api.onfinality.io/public-ws",
+  archive: lookupArchive("astar")[0].url,
 });
 
-processor.setTypesBundle("moonbeam");
-
-processor.addPreHook({ range: { from: 0, to: 0 } }, async (ctx) => {
-  await ctx.store.save(createContractEntity());
-});
-
+processor.setTypesBundle("astar");
 processor.addEvmLogHandler(
-  contract.address,
+  "0xd89dEa2daC8Fb73F4107C2cbeA5Eb36dab511F64".toLowerCase(),
   {
-    filter: [erc721.events["Transfer(address,address,uint256)"].topic],
+      filter: [events["InvestDeposited(uint256)"].topic],
+      range: {from: 864889}
   },
-  contractLogsHandler
+  bankSafeHandler
 );
 
-export async function contractLogsHandler(
+processor.run();
+
+export async function bankSafeHandler(
   ctx: EvmLogHandlerContext
 ): Promise<void> {
-  const transfer =
-    erc721.events["Transfer(address,address,uint256)"].decode(ctx);
-
-  let from = await ctx.store.get(Owner, transfer.from);
-  if (from == null) {
-    from = new Owner({ id: transfer.from, balance: 0n });
-    await ctx.store.save(from);
-  }
-
-  let to = await ctx.store.get(Owner, transfer.to);
-  if (to == null) {
-    to = new Owner({ id: transfer.to, balance: 0n });
-    await ctx.store.save(to);
-  }
-
-  let token = await ctx.store.get(Token, transfer.tokenId.toString());
-  if (token == null) {
-    token = new Token({
-      id: transfer.tokenId.toString(),
-      uri: await contract.tokenURI(transfer.tokenId),
-      contract: await getContractEntity(ctx),
-      owner: to,
-    });
-    await ctx.store.save(token);
-  } else {
-    token.owner = to;
-    await ctx.store.save(token);
-  }
-
+  const investDeposit = events["InvestDeposited(uint256)"].decode(ctx);
   await ctx.store.save(
-    new Transfer({
-      id: ctx.txHash,
-      token,
-      from,
-      to,
-      timestamp: BigInt(ctx.substrate.block.timestamp),
-      block: ctx.substrate.block.height,
-      transactionHash: ctx.txHash,
-    })
-  );
+      new Investement({
+          id: ctx.txHash,
+          timestamp: BigInt(ctx.substrate.block.timestamp),
+          value: (Number(investDeposit.amount) / 1e6)
+      })
+  )
 }
-
-processor.run();
